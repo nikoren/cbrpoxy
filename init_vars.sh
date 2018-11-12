fe2a447c053bbea837507f4ce1bb66f4608c794e
nikolai <nikoren@gmail.com>
2018-11-12 09:58:49 -0500

    Initial version of circuit breaker proxy

---
+++ init_vars.sh
@@ -0,0 +1,6 @@
+export GOPATH=/Users/niko/dev/go
+export GOBIN=$GOPATH/github.com/nikoren/cbproxy/bin
+export PORT=1330
+export A_CONDITION_URL="https://jsonplaceholder.typicode.com/todos/1"
+export B_CONDITION_URL="https://jsonplaceholder.typicode.com/todos/2"
+export DEFAULT_CONDITION_URL="https://jsonplaceholder.typicode.com/todos/3"
